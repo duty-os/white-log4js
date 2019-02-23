0d81f784af8206b62bc9972ad88928f3728f34b4
buhe <buhe.bh@antfin.com>
2019-02-23 10:40:08 +0800

    add perf script

---
+++ log.rewrite.perf.ts
@@ -0,0 +1,13 @@
+import "./log";
+console.time("before");
+
+for (let i = 0; i < 100; i++) {
+    console.info("info");
+}
+
+for (let i = 0; i < 100; i++) {
+    console.error(new Error("fun"));
+}
+
+
+console.timeEnd("before");
